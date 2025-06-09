b8858d5c2745bfb4770b745f8a33b0c80131e454
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-06-09 15:26:45 +0530

    deploy

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from "axios";
-// const BASE_URL = "https://chat-website-backend-tsau.onrender.com/api/v1";
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = "https://chat-website-backend-tsau.onrender.com/api/v1";
+// const BASE_URL = "http://localhost:3000/api/v1";
 const API = axios.create({
   baseURL: `${BASE_URL}`, // Replace with your base URL
   headers: {
