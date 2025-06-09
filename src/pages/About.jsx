4c1ecdd8ffbe2b7a37da7d09d47e8e0d759ff5c1
MkumarMonu <148427409+MkumarMonu@users.noreply.github.com>
2025-06-09 15:35:15 +0530

    error resolve again

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import UserCard from "../components/userCard";
+import UserCard from "../components/UserCard";
 import {
   acceptRequestAPI,
   getRequestAPI,
